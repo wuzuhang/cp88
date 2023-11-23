5d25bb446a48b824d496e9f9afed2fd5a1a06c75
wuzuhang <wu.zuhang@nets-tech.com>
2023-11-23 11:09:19 +0800

    save

--- src/views/other.vue
+++ src/views/other.vue
@@ -1,24 +1,44 @@
 <template>
-  <div class="list">
+  <div class="list" v-loading="state.loading">
     <div class="header">
-      <el-input-number
-        v-model="state.startnum"
-        :min="0"
-        @change="handleChange"
-      />
+      <div class="marginbtn">
+        <span>上一期</span>&nbsp;&nbsp;
+        <el-input-number
+          v-model="state.startnum"
+          :min="0"
+          @change="handleChange"
+        />
+        &nbsp;&nbsp;<span>下一期</span>
+      </div>
+      <div class="marginbtn sample" v-show="state.time">
+        {{ state.time }}:
+        <span class="redhit" v-for="item in state.redhitd">{{ item }}</span>
+        <span class="bluehit">{{ state.bluehitd }}</span>
+      </div>
+
+      <el-button type="primary" class="marginbtn" @click="sampleInit"
+        >随机生成开奖号码</el-button
+      >
+      <div class="marginbtn sample" v-show="state.redlistSort.length > 0">
+        <span class="redhit" v-for="item in state.redlistSort">{{ item }}</span>
+        <span class="bluehit">{{ state.bluehit }}</span>
+      </div>
+      <el-button type="primary" class="marginbtn" @click="reload"
+        >重置</el-button
+      >
     </div>
     <div class="border">
-      <div class="row" v-show="false">
+      <div class="row">
         <div
           class="red"
-          :class="{ hidden: index == 0 }"
-          v-for="(item, index) in 7"
+          :class="{ blue: index == 7, hidden: index == 0 }"
+          v-for="(item, index) in 8"
         >
           {{ index }}
         </div>
       </div>
       <div class="cow">
-        <div class="cow-red">
+        <div class="cow-red redNo">
           <div class="red" v-for="(item, index) in 33">
             {{ item }}
           </div>
@@ -34,8 +54,29 @@
               class4: items.order == 4,
             }"
             v-for="(items, indexs) in item"
+            @click="numclick(items)"
           >
-            <span :class="{hitred: items.hit,}">{{items.rednum}}|{{items.order}}</span>
+            <span :class="{ hitred: items.hit }"
+              >{{ items.rednum }}</span
+            >
+          </div>
+        </div>
+        <div class="cow-red">
+          <div
+            class="setred"
+            v-for="(items, indexs) in state.resultblue"
+            :class="{
+              class0: items.order == 0,
+              class1: items.order == 1,
+              class2: items.order == 2,
+              class3: items.order == 3,
+              class4: items.order == 4,
+            }"
+            @click="numclick(items)"
+          >
+            <span :class="{ hitred: items.hit }"
+              >{{ items.bluenum }}</span
+            >
           </div>
         </div>
       </div>
@@ -51,10 +92,15 @@ const state = reactive({
   xlsxList: [],
   loading: false,
   redlist: [],
+  redlistSort: [],
   numlist: [],
   result: [],
   resultblue: [],
-  class0:[]
+  class0: [],
+  bluehit: "",
+  time: "",
+  redhitd: [],
+  bluehitd: "",
 });
 onMounted(() => {
   init();
@@ -74,7 +120,6 @@ const init = () => {
       numlist.push(item.slice(9, 15));
     });
     setred();
-    sampleInit();
     state.loading = false;
   });
 };
@@ -91,30 +136,48 @@ const sample = (list) => {
   }
 };
 const sampleInit = () => {
+  state.time = "";
+  state.startnum = 0;
+  setred();
   let redlist = [];
   let bluelist = Math.floor(Math.random() * 16) + 1;
   for (var i = 1; i <= 33; i++) {
     redlist.push(i);
   }
   state.redlist = [];
+  state.bluehit = bluelist;
   sample(redlist);
-  let list = [];
-  list.push(state.redlist);
-  return list;
+  state.redlistSort = JSON.parse(JSON.stringify(state.redlist)).sort(
+    (a, b) => a - b
+  );
+  state.result.forEach((item, index) => {
+    item.forEach((items) => {
+      items.hit = false;
+      if (items.rednum == state.redlist[index]) {
+        items.hit = true;
+      }
+    });
+  });
+  state.resultblue.forEach((item) => {
+    item.hit = false;
+    if (item.bluenum == bluelist) {
+      item.hit = true;
+    }
+  });
 };
 const setred = () => {
   let redlist = [];
   state.xlsxList.forEach((item) => {
     redlist.push(item.slice(9, 15));
   });
-  console.log("zzzzzzzzzzzzz", redlist);
   let num = 1;
   let hitlist = state.startnum == 0 ? [] : redlist[state.startnum - 1];
   var intArray = hitlist.map(function (str) {
     return parseInt(str, 10);
   });
-  console.log(intArray);
-  state.class0 = []
+  state.redhitd = JSON.parse(JSON.stringify(intArray)).sort((a, b) => a - b);
+
+  state.class0 = [];
   getrednum(
     redlist.slice(state.startnum, (33 + state.startnum) * num),
     intArray
@@ -144,9 +207,18 @@ const getrednum = (redlist, hitlist) => {
   let result5 = countOccurrences(red5, hitlist[4]);
   let result6 = countOccurrences(red6, hitlist[5]);
   state.result = [result1, result2, result3, result4, result5, result6];
-  let class0 = Array.from(new Set(state.class0))
-  class0.sort((a,b)=>a-b)
-  console.log(class0);
+
+  let bluelist = [];
+  state.xlsxList.forEach((item) => {
+    bluelist.push(item[8]);
+  });
+  let num = 1;
+  let resultblue = countOccurrencesblue(
+    bluelist.slice(state.startnum, (16 + state.startnum) * num)
+  );
+  state.resultblue = resultblue;
+  let class0 = Array.from(new Set(state.class0));
+  class0.sort((a, b) => a - b);
 };
 
 function countOccurrences(arr, hitlist) {
@@ -165,21 +237,87 @@ function countOccurrences(arr, hitlist) {
       hit: hitlist == index + 1,
       color: 0,
     });
-    if(item==0){
-      state.class0.push(index + 1)
+    if (item == 0) {
+      state.class0.push(index + 1);
+    }
+  });
+  list.sort((a, b) => a.order - b.order);
+  return list;
+}
+function countOccurrencesblue(arr) {
+  let hitlist =
+    state.startnum == 0 ? 0 : parseInt(state.xlsxList[state.startnum - 1][8]);
+  state.bluehitd = hitlist;
+  state.numlist = [];
+  for (var i = 0; i < 16; i++) {
+    state.numlist.push(0);
+  }
+  let list = [];
+  arr.forEach((item) => {
+    state.numlist[item - 1] += 1;
+  });
+  state.numlist.forEach((item, index) => {
+    list.push({
+      bluenum: index + 1,
+      order: item,
+      hit: hitlist == index + 1,
+      color: 0,
+    });
+    if (item == 0) {
+      state.class0.push(index + 1);
     }
   });
   list.sort((a, b) => a.order - b.order);
   return list;
 }
 const handleChange = (num) => {
+  if (num == 0) {
+    state.time = "";
+  } else {
+    state.time = state.xlsxList[state.startnum - 1][1];
+  }
+  state.redlistSort = [];
   setred();
+};
+const numclick = (a) => {
+  a.hit = !a.hit;
 };
 </script>
 
 <style lang="less" scoped>
 .list {
   // padding: 2vw;
+  .header {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    .marginbtn {
+      margin-bottom: 10px;
+    }
+    .sample {
+      .redhit {
+        display: inline-block;
+        width: 24px;
+        height: 24px;
+        background-color: rgb(245, 56, 56);
+        color: #ffffff;
+        border-radius: 50%;
+        margin-right: 5px;
+        text-align: center;
+        line-height: 24px;
+      }
+      .bluehit {
+        display: inline-block;
+        width: 24px;
+        height: 24px;
+        background-color: rgb(73, 79, 249);
+        color: #ffffff;
+        border-radius: 50%;
+        text-align: center;
+        line-height: 24px;
+      }
+    }
+  }
   .border {
     .row {
       display: flex;
@@ -202,12 +340,23 @@ const handleChange = (num) => {
     .cow {
       display: flex;
       justify-content: space-around;
+      .redNo {
+        border: 1px solid #333333;
+        background-color: rgb(255, 127, 127);
+        font-weight: bold;
+        color: #333333;
+        max-width: 12vw;
+      }
       .cow-red {
         width: 100%;
+        .red {
+          border-bottom: 1px solid #333333;
+          text-align: center;
+        }
       }
       .setred {
         text-align: center;
-        span{
+        span {
           display: inline-block;
         }
         // background-color: aquamarine;
@@ -230,7 +379,15 @@ const handleChange = (num) => {
 
       .hitred {
         width: 60%;
-        background-color: red;
+        background-color: rgb(245, 56, 56);
+        color: #ffffff;
+        font-weight: bold;
+      }
+      .hitblue {
+        width: 60%;
+        background-color: rgb(73, 79, 249);
+        color: #ffffff;
+        font-weight: bold;
       }
     }
   }
